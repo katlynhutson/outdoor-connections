2d7842ecabd9852461614042a546bb2785ade887
katlynhutson <kmhutson5@gmail.com>
2022-03-16 15:47:22 -0500

    Add links to components to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav(props) {
 	return (
 		<nav>
 			<ul>
-				<li></li>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+				<li>
+					<Link to='/attendance'>Attendance</Link>
+				</li>
+				<li>
+					<Link to='/blog'>Blog</Link>
+				</li>
+				<li>
+					<Link to='/camping'>Camping</Link>
+				</li>
+				<li>
+					<Link to='/donations'>Donations</Link>
+				</li>
+				<li>
+					<Link to='/fit2hike'>Fit 2 Hike</Link>
+				</li>
+				<li>
+					<Link to='/guidelines'>Guidelines</Link>
+				</li>
+				<li>
+					<Link to='/lnt'>Leave No Trace</Link>
+				</li>
+				<li>
+					<Link to='/pace'>Pace</Link>
+				</li>
+				<li>
+					<Link to='/supplies'>Supplies</Link>
+				</li>
+				<li>
+					<Link to='/teamleaders'>Team Leaders</Link>
+				</li>
+				<li>
+					<Link to='/trailrules'>Trail Rules</Link>
+				</li>
+				<li>
+					<Link to='/waiver'>Waiver</Link>
+				</li>
 			</ul>
 		</nav>
 	);
