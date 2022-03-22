b7aa1ef61adca6e80c18b6539dc6d7932a278aa1
katlynhutson <kmhutson5@gmail.com>
2022-03-22 17:41:01 -0500

    About page partial information

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function About(props) {
-	return <div></div>;
+	return (
+		<>
+			<div>
+				Outdoor Connection is a group for adult women that enjoy getting
+				together for hiking, athletic walking, participating in races, camping,
+				and additional fitness or outdoor activities. Originally founded in 2007
+				as a group for walkers, we have morphed over the years to include more
+				hiking and a variety of other outdoor activities. Many of our activities
+				are right here in town! If you like spending time outside, getting in a
+				good workout, and making new friends you will enjoy this group.
+			</div>
+			<div>
+				We are based in Dallas Texas. We visit many trails around the DFW area.
+				Some of our favorites include White Rock Lake, Cedar Ridge Preserve,
+				River Legacy Park, Northshore Trail at Lake Grapevine, Eagle Mountain
+				Lake, and more. We walk both paved trails and natural surface hiking
+				trails. Occasionally we head out of town for a day hike or campout at
+				one of the nearby state parks. We may also schedule additional types of
+				outdoor, fitness, or social activities.
+			</div>
+			<div></div>
+		</>
+	);
 }
 
 export default About;
